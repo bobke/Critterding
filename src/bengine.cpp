df781c7d3d3589dad01296f8c6378bbbfc63a5da
bobke <bobke@2ee6282c-0497-4b59-8b7c-bcac2105c061>
2008-02-08 23:50:56 +0000

    application name change to critterding

--- src/bengine.cpp
+++ src/bengine.cpp
@@ -19,7 +19,7 @@ int main(int argc, char *argv[])
 
 	cerr << "Starting application" << endl;
 	GLWindow glwindow;
-	glwindow.create("B-Engine Evolution :)", 320, 300, 24, False);
+	glwindow.create("Critterding 0.01", 320, 300, 24, False);
 
 		Evolution mainscene;
 		glwindow.runGLScene(mainscene);
