62a0198a925c6afb561964409c47cf576d0054da
bobke <bobke@2ee6282c-0497-4b59-8b7c-bcac2105c061>
2009-09-20 20:41:14 +0000

    body_percentmutateeffectchangeconstraintposition_slightly body_percentmutateeffectchangeconstraintangles_slightly body_percentmutateeffectchangeconstraintlimits_slightly
    git-svn-id: svn://84.194.112.164/bengine@324 2ee6282c-0497-4b59-8b7c-bcac2105c061

--- src/scenes/entities/body.cpp
+++ src/scenes/entities/body.cpp
@@ -404,7 +404,22 @@ void Body::mutate(unsigned int runs)
 {
 	for ( unsigned int i=0; i < runs; i++ )
 	{
-		unsigned int mode = randgen->Instance()->get(1,100);
+		unsigned int tsum = 	settings->getCVar("body_percentmutateeffectaddbodypart")
+					+ settings->getCVar("body_percentmutateeffectremovebodypart")
+					+ settings->getCVar("body_percentmutateeffectresizebodypart")
+					+ settings->getCVar("body_percentmutateeffectresizebodypart_slightly")
+					+ settings->getCVar("body_percentmutateeffectresizehead")
+					+ settings->getCVar("body_percentmutateeffectresizehead_slightly")
+					+ settings->getCVar("body_percentmutateeffectchangeconstraintlimits")
+					+ settings->getCVar("body_percentmutateeffectchangeconstraintlimits_slightly")
+					+ settings->getCVar("body_percentmutateeffectchangeconstraintangles")
+					+ settings->getCVar("body_percentmutateeffectchangeconstraintangles_slightly")
+					+ settings->getCVar("body_percentmutateeffectchangeconstraintposition")
+					+ settings->getCVar("body_percentmutateeffectchangeconstraintposition_slightly")
+					+ settings->getCVar("body_percentmutateeffectrepositionhead")
+				;
+
+		unsigned int mode = randgen->Instance()->get(1,tsum);
 		
 		// ADD BODYPART
 			unsigned int modesum = settings->getCVar("body_percentmutateeffectaddbodypart");
@@ -556,7 +571,7 @@ void Body::mutate(unsigned int runs)
 				continue;
 			}
 
-		// RESIZE MOUTH
+		// RESIZE HEAD
 			modesum += settings->getCVar("body_percentmutateeffectresizehead");
 			if ( mode <= modesum )
 			{
@@ -581,7 +596,7 @@ void Body::mutate(unsigned int runs)
 				continue;
 			}
 
-		// RESIZE BODYPART SLIGHTLY
+		// RESIZE HEAD SLIGHTLY
 			modesum += settings->getCVar("body_percentmutateeffectresizehead_slightly");
 			if ( mode <= modesum )
 			{
@@ -653,6 +668,32 @@ void Body::mutate(unsigned int runs)
 				continue;
 			}
 	
+		// CHANGE CONSTRAINT LIMITS SLIGHTLY
+			modesum += settings->getCVar("body_percentmutateeffectchangeconstraintlimits_slightly");
+			if ( mode <= modesum )
+			{
+// 				cerr << "changing constraint limits" << endl;
+
+				unsigned int cid = randgen->Instance()->get( 0, archConstraints.size()-1 );
+				archConstraint* co = &archConstraints[cid];
+				
+				unsigned int direction = randgen->Instance()->get(0,1);
+
+				if ( direction == 0 )
+					co->limit_1 += 0.01f;
+				else
+					co->limit_1 -= 0.01f;
+				// check limits limits, ya
+				if ( co->limit_1 > 0.0f )
+					co->limit_1 = 0.0f;
+				if ( co->limit_1 < -0.7853f )
+					co->limit_1 = -0.7853f;
+
+				co->limit_2 = -1.0f * co->limit_1;
+
+// 				cerr << "done changing constraint limits" << endl;
+				continue;
+			}
 		// CHANGE CONSTRAINT ANGLES
 			modesum += settings->getCVar("body_percentmutateeffectchangeconstraintangles");
 			if ( mode <= modesum )
@@ -668,6 +709,87 @@ void Body::mutate(unsigned int runs)
 				co->rot_x_2		= ((float)randgen->Instance()->get( 0, 3141 ) - 1571) / 1000;
 				co->rot_y_2		= ((float)randgen->Instance()->get( 0, 3141 ) - 1571) / 1000;
 				co->rot_z_2		= ((float)randgen->Instance()->get( 0, 3141 ) - 1571) / 1000;
+
+// 				cerr << "done changing constraint angles" << endl;
+				continue;
+			}
+
+		// CHANGE CONSTRAINT ANGLES SLIGHTLY
+			modesum += settings->getCVar("body_percentmutateeffectchangeconstraintangles_slightly");
+			if ( mode <= modesum )
+			{
+// 				cerr << "changing constraint angles" << endl;
+
+				unsigned int cid = randgen->Instance()->get( 0, archConstraints.size()-1 );
+				archConstraint* co = &archConstraints[cid];
+
+				unsigned int XYZ = randgen->Instance()->get(0,2);
+				unsigned int direction = randgen->Instance()->get(0,1);
+				if ( direction == 0 )
+				{
+					if ( XYZ == 0 )
+						co->rot_x_1 += 0.01f;
+					else if ( XYZ == 1 )
+						co->rot_y_1 += 0.01f;
+					else
+						co->rot_z_1 += 0.01f;
+				}
+				else
+				{
+					if ( XYZ == 0 )
+						co->rot_x_1 -= 0.01f;
+					else if ( XYZ == 1 )
+						co->rot_y_1 -= 0.01f;
+					else
+						co->rot_z_1 -= 0.01f;
+				}
+
+				XYZ = randgen->Instance()->get(0,2);
+				direction = randgen->Instance()->get(0,1);
+				if ( direction == 0 )
+				{
+					if ( XYZ == 0 )
+						co->rot_x_2 += 0.01f;
+					else if ( XYZ == 1 )
+						co->rot_y_2 += 0.01f;
+					else
+						co->rot_z_2 += 0.01f;
+				}
+				else
+				{
+					if ( XYZ == 0 )
+						co->rot_x_2 -= 0.01f;
+					else if ( XYZ == 1 )
+						co->rot_y_2 -= 0.01f;
+					else
+						co->rot_z_2 -= 0.01f;
+				}
+
+				if ( co->rot_x_1 < -0.1571f )
+					co->rot_x_1 = -0.1571f;
+				if ( co->rot_x_1 > 0.1571f )
+					co->rot_x_1 = 0.1571f;
+				if ( co->rot_y_1 < -0.1571f )
+					co->rot_y_1 = -0.1571f;
+				if ( co->rot_y_1 > 0.1571f )
+					co->rot_y_1 = 0.1571f;
+				if ( co->rot_z_1 < -0.1571f )
+					co->rot_z_1 = -0.1571f;
+				if ( co->rot_z_1 > 0.1571f )
+					co->rot_z_1 = 0.1571f;
+
+				if ( co->rot_x_2 < -0.1571f )
+					co->rot_x_2 = -0.1571f;
+				if ( co->rot_x_2 > 0.1571f )
+					co->rot_x_2 = 0.1571f;
+				if ( co->rot_y_2 < -0.1571f )
+					co->rot_y_2 = -0.1571f;
+				if ( co->rot_y_2 > 0.1571f )
+					co->rot_y_2 = 0.1571f;
+				if ( co->rot_z_2 < -0.1571f )
+					co->rot_z_2 = -0.1571f;
+				if ( co->rot_z_2 > 0.1571f )
+					co->rot_z_2 = 0.1571f;
 
 // 				cerr << "done changing constraint angles" << endl;
 				continue;
@@ -691,12 +813,168 @@ void Body::mutate(unsigned int runs)
 					connID2 = findBodypart(co->id_2);
 
 				// pick one of 2 bodies to reconnect
-				unsigned int body = randgen->Instance()->get( 0, 1 );
+				unsigned int body1or2 = randgen->Instance()->get( 1, 2 );
 
-				if ( body == 0 )
+				if ( body1or2 == 1 )
 					randomConstraintPosition(co, 1, connID1);
 				else
 					randomConstraintPosition(co, 2, connID2);
+
+// 				cerr << "done changing constraint position" << endl;
+				continue;
+			}
+
+		// REPOSITION A CONSTRAINT SLIGHTLY
+			modesum += settings->getCVar("body_percentmutateeffectchangeconstraintposition_slightly");
+			if ( mode <= modesum )
+			{
+// 				cerr << "changing constraint position" << endl;
+
+				unsigned int cid = randgen->Instance()->get( 0, archConstraints.size()-1 );
+				archConstraint* co = &archConstraints[cid];
+
+				int connID1 = findBodypart(co->id_1);
+				int connID2;
+
+				if ( co->isMouthConstraint )
+					connID2 = findMouth(co->id_2);
+				else
+					connID2 = findBodypart(co->id_2);
+
+				// pick one of 2 bodies to reconnect
+				unsigned int body1or2 = randgen->Instance()->get( 1, 2 );
+				unsigned int direction = randgen->Instance()->get( 0, 1 );
+				unsigned int axis1or2 = randgen->Instance()->get( 0, 1 );
+
+				if ( body1or2 == 1 ) {
+					// now we know the plane to connect to, determine positions
+					if ( co->XYZ == 0 ) { // X
+						if ( direction == 0 ) {
+							if ( axis1or2 == 0 )	co->pos_y_1 += archBodyparts[connID1].y / 100000;
+							else			co->pos_z_1 += archBodyparts[connID1].z / 100000;
+						} else {
+							if ( axis1or2 == 0 )	co->pos_y_1 -= archBodyparts[connID1].y / 100000;
+							else			co->pos_z_1 -= archBodyparts[connID1].z / 100000;
+						}
+					}
+					else if ( co->XYZ == 1 ) { // Y
+						if ( direction == 0 ) {
+							if ( axis1or2 == 0 )	co->pos_x_1 += archBodyparts[connID1].x / 100000;
+							else			co->pos_z_1 += archBodyparts[connID1].z / 100000;
+						} else {
+							if ( axis1or2 == 0 )	co->pos_x_1 -= archBodyparts[connID1].x / 100000;
+							else			co->pos_z_1 -= archBodyparts[connID1].z / 100000;
+						}
+					}
+					else { // Z
+						if ( direction == 0 ) {
+							if ( axis1or2 == 0 )	co->pos_x_1 += archBodyparts[connID1].x / 100000;
+							else			co->pos_y_1 += archBodyparts[connID1].y / 100000;
+						} else {
+							if ( axis1or2 == 0 )	co->pos_x_1 -= archBodyparts[connID1].x / 100000;
+							else			co->pos_y_1 -= archBodyparts[connID1].y / 100000;
+						}
+					}
+					if ( co->pos_x_1 < archBodyparts[connID1].x/-2000 )
+						co->pos_x_1 = archBodyparts[connID1].x/-2000;
+					if ( co->pos_x_1 > archBodyparts[connID1].x/2000 )
+						co->pos_x_1 = archBodyparts[connID1].x/2000;
+					if ( co->pos_y_1 < archBodyparts[connID1].y/-2000 )
+						co->pos_y_1 = archBodyparts[connID1].y/-2000;
+					if ( co->pos_y_1 > archBodyparts[connID1].y/2000 )
+						co->pos_y_1 = archBodyparts[connID1].y/2000;
+					if ( co->pos_z_1 < archBodyparts[connID1].z/-2000 )
+						co->pos_z_1 = archBodyparts[connID1].z/-2000;
+					if ( co->pos_z_1 > archBodyparts[connID1].z/2000 )
+						co->pos_z_1 = archBodyparts[connID1].z/2000;
+				}
+				else {
+					if ( !co->isMouthConstraint ) {
+						// now we know the plane to connect to, determine positions
+						if ( co->XYZ == 0 ) { // X
+							if ( direction == 0 ) {
+								if ( axis1or2 == 0 )	co->pos_y_2 += archBodyparts[connID2].y / 100000;
+								else			co->pos_z_2 += archBodyparts[connID2].z / 100000;
+							} else {
+								if ( axis1or2 == 0 )	co->pos_y_2 -= archBodyparts[connID2].y / 100000;
+								else			co->pos_z_2 -= archBodyparts[connID2].z / 100000;
+							}
+						}
+						else if ( co->XYZ == 1 ) { // Y
+							if ( direction == 0 ) {
+								if ( axis1or2 == 0 )	co->pos_x_2 += archBodyparts[connID2].x / 100000;
+								else			co->pos_z_2 += archBodyparts[connID2].z / 100000;
+							} else {
+								if ( axis1or2 == 0 )	co->pos_x_2 -= archBodyparts[connID2].x / 100000;
+								else			co->pos_z_2 -= archBodyparts[connID2].z / 100000;
+							}
+						}
+						else { // Z
+							if ( direction == 0 ) {
+								if ( axis1or2 == 0 )	co->pos_x_2 += archBodyparts[connID2].x / 100000;
+								else			co->pos_y_2 += archBodyparts[connID2].y / 100000;
+							} else {
+								if ( axis1or2 == 0 )	co->pos_x_2 -= archBodyparts[connID2].x / 100000;
+								else			co->pos_y_2 -= archBodyparts[connID2].y / 100000;
+							}
+						}
+						if ( co->pos_x_2 < archBodyparts[connID2].x/-2000 )
+							co->pos_x_2 = archBodyparts[connID2].x/-2000;
+						if ( co->pos_x_2 > archBodyparts[connID2].x/2000 )
+							co->pos_x_2 = archBodyparts[connID2].x/2000;
+						if ( co->pos_y_2 < archBodyparts[connID2].y/-2000 )
+							co->pos_y_2 = archBodyparts[connID2].y/-2000;
+						if ( co->pos_y_2 > archBodyparts[connID2].y/2000 )
+							co->pos_y_2 = archBodyparts[connID2].y/2000;
+						if ( co->pos_z_2 < archBodyparts[connID2].z/-2000 )
+							co->pos_z_2 = archBodyparts[connID2].z/-2000;
+						if ( co->pos_z_2 > archBodyparts[connID2].z/2000 )
+							co->pos_z_2 = archBodyparts[connID2].z/2000;
+					}
+					else {
+						if ( co->XYZ == 0 ) { // X
+							if ( direction == 0 ) {
+								if ( axis1or2 == 0 )	co->pos_y_2 += archMouths[connID2].y / 100000;
+								else			co->pos_z_2 += archMouths[connID2].z / 100000;
+							} else {
+								if ( axis1or2 == 0 )	co->pos_y_2 -= archMouths[connID2].y / 100000;
+								else			co->pos_z_2 -= archMouths[connID2].z / 100000;
+							}
+						}
+						else if ( co->XYZ == 1 ) { // Y
+							if ( direction == 0 ) {
+								if ( axis1or2 == 0 )	co->pos_x_2 += archMouths[connID2].x / 100000;
+								else			co->pos_z_2 += archMouths[connID2].z / 100000;
+							} else {
+								if ( axis1or2 == 0 )	co->pos_x_2 -= archMouths[connID2].x / 100000;
+								else			co->pos_z_2 -= archMouths[connID2].z / 100000;
+							}
+						}
+						else { // Z
+							if ( direction == 0 ) {
+								if ( axis1or2 == 0 )	co->pos_x_2 += archMouths[connID2].x / 100000;
+								else			co->pos_y_2 += archMouths[connID2].y / 100000;
+							} else {
+								if ( axis1or2 == 0 )	co->pos_x_2 -= archMouths[connID2].x / 100000;
+								else			co->pos_y_2 -= archMouths[connID2].y / 100000;
+							}
+						}
+						if ( co->pos_x_2 < archMouths[connID2].x/-2000 )
+							co->pos_x_2 = archMouths[connID2].x/-2000;
+						if ( co->pos_x_2 > archMouths[connID2].x/2000 )
+							co->pos_x_2 = archMouths[connID2].x/2000;
+						if ( co->pos_y_2 < archMouths[connID2].y/-2000 )
+							co->pos_y_2 = archMouths[connID2].y/-2000;
+						if ( co->pos_y_2 > archMouths[connID2].y/2000 )
+							co->pos_y_2 = archMouths[connID2].y/2000;
+						if ( co->pos_z_2 < archMouths[connID2].z/-2000 )
+							co->pos_z_2 = archMouths[connID2].z/-2000;
+						if ( co->pos_z_2 > archMouths[connID2].z/2000 )
+							co->pos_z_2 = archMouths[connID2].z/2000;
+					}
+				}
+					
+// 					randomConstraintPosition(co, 2, connID2);
 
 // 				cerr << "done changing constraint position" << endl;
 				continue;
