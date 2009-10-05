66880fce301248f73336543646205266166fdaef
bobke <bobke@2ee6282c-0497-4b59-8b7c-bcac2105c061>
2009-10-05 01:11:15 +0000

    added vector2i object

--- src/gui/panel.cpp
+++ src/gui/panel.cpp
@@ -12,6 +12,24 @@ void Panel::draw()
 	drawChildren();
 }
 
+void Panel::drawBackground()
+{
+/*	glBlendFunc(GL_SRC_ALPHA,GL_ONE_MINUS_SRC_ALPHA);
+	glEnable(GL_BLEND);
+	glColor4f(0.05f, 0.05f, 0.05f, 0.9f);
+	glBegin(GL_QUADS);
+		glVertex2f(halfwidth-halfboxwidth, halfheight+halfboxheight);
+		glVertex2f(halfwidth-halfboxwidth, halfheight-halfboxheight);
+		glVertex2f(halfwidth+halfboxwidth, halfheight-halfboxheight);
+		glVertex2f(halfwidth+halfboxwidth, halfheight+halfboxheight);
+	glEnd();
+	glDisable(GL_BLEND);*/
+}
+
+void Panel::drawBorders()
+{
+}
+
 Panel::~Panel()
 {
 }
---
+++ src/math/vector2i.cpp
@@ -0,0 +1,11 @@
+#include "vector2i.h"
+
+Vector2i::Vector2i()
+{
+	x = 0;
+	y = 0;
+}
+
+Vector2i::~Vector2i()
+{
+}
