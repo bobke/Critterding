ae781a513aeb0c8ea74b82cdee66beaea4cc9b27
bobke <bobke@2ee6282c-0497-4b59-8b7c-bcac2105c061>
2009-11-22 17:49:45 +0000

    vector2f

---
+++ src/math/vector2f.cpp
@@ -0,0 +1,17 @@
+#include "vector2f.h"
+
+Vector2f::Vector2f()
+{
+	x = 0.0f;
+	y = 0.0f;
+}
+
+Vector2f::Vector2f(const float& xv, const float& yv)
+{
+	x = xv;
+	y = yv;
+}
+
+Vector2f::~Vector2f()
+{
+}
